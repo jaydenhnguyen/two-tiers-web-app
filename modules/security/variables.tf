69b1f4b7a01e3238b5e40af1b97f62c3cd1b68e1
Jayden H Nguyen <nguyenhuuduchuy2712@gmail.com>
2026-03-29 15:30:35 -0400

    🧱 add SG for alb, bastion and VMs

---
+++ modules/security/variables.tf
@@ -0,0 +1,26 @@
+variable "project_name" {
+  description = "Project or group name"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "VPC ID where security groups will be created"
+  type        = string
+}
+
+variable "allowed_ssh_cidr_blocks" {
+  description = "CIDR blocks allowed to SSH to bastion host"
+  type        = list(string)
+  default     = []
+}
+
+variable "tags" {
+  description = "Common tags"
+  type        = map(string)
+  default     = {}
+}
